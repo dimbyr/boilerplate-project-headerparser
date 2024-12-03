b6f5774c3fe014b895554ecadf935f0cab47de59
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-12-03 09:09:17 +0300

    Done

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ "api": requestIp.getClientIp(req) });
 });
 
+// your first API endpoint...
+app.get('/api/whoami', function (req, res) {
+  res.json({ "ipaddress": requestIp.getClientIp(res), "language": "l", "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
